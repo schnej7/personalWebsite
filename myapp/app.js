4756c80d0e5a0d6a32bae5a5722059e349c2238e
Jerry Schneider <schnej7@gmail.com>
2015-02-08 01:02:35 +0000

    restructure

--- myapp/app.js
+++ myapp/app.js
@@ -1,41 +1,40 @@
 var express = require('express')
 var app = express()
-app.set('views', './views')
-app.set('view engine', 'jade')
+
 var moment = require('moment');
 var favicon = require('serve-favicon');
 var wordGame = require('./my_modules/wordGame.js');
+
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB = require('./app/database.js');
+
+//mongoose.connect(configDB.url); // connect to our database
+
+// require('./config/passport')(passport); // pass passport for configuration
+
 app.use(express.static(__dirname + '/public'));
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Jerry Schneider', year: (moment(Date.now())).format('YYYY')});
-})
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
 
-app.get('/resume', function (req, res) {
-  res.render('resume', { title: "Jerry Schneider's Resume", year: (moment(Date.now())).format('YYYY')});
-})
+app.set('views', './views')
+app.set('view engine', 'jade')
 
-app.get('/emulator', function (req, res) {
-  res.render('emulator', { title: "Jerry Schneider's Resume", year: (moment(Date.now())).format('YYYY')});
-})
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-app.get('/wordGame', function (req, res) {
-  res.render('wordGame', { title: "Jerry Word Game", year: (moment(Date.now())).format('YYYY')});
-})
-
-app.get('/wordGameAjax', function (req, res) {
-  wordGame.handleRequest(req, res);
-})
-
-app.get('/secrets', function (req, res) {
-  res.render('secrets', { title: "Secrets Links", year: (moment(Date.now())).format('YYYY')});
-})
-
-// accept POST request on the homepage
-app.post('/', function (req, res) {
-  res.send('Got a POST request');
-})
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 var server = app.listen(80, function () {
 
